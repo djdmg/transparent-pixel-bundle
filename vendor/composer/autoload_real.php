8818c857fa421e701b6bb40755b5782591119bf7
djdmg <dimitrigrard@gmail.com>
2025-08-15 13:05:22 +0700

    Fix symfony 6.4

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -31,8 +31,19 @@ class ComposerAutoloaderInitcc42a8d7e3668f6ebc214f7b0090cd73
         require __DIR__ . '/autoload_static.php';
         call_user_func(\Composer\Autoload\ComposerStaticInitcc42a8d7e3668f6ebc214f7b0090cd73::getInitializer($loader));
 
-        $loader->setClassMapAuthoritative(true);
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitcc42a8d7e3668f6ebc214f7b0090cd73::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
