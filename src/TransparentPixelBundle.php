436b23f45bc4a16a82dc024bff18fa3ddbc93a83
djdmg <dimitrigrard@gmail.com>
2025-08-15 14:59:49 +0700

    fix pixel repository

--- src/TransparentPixelBundle.php
+++ src/TransparentPixelBundle.php
@@ -4,7 +4,23 @@ declare(strict_types=1);
 namespace Djdmg\TransparentPixelBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
 
 final class TransparentPixelBundle extends Bundle
 {
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        if (class_exists(DoctrineOrmMappingsPass::class)) {
+            $mappings = [
+                __DIR__.'/Entity' => 'Djdmg\\TransparentPixelBundle\\Entity',
+            ];
+            $container->addCompilerPass(
+                DoctrineOrmMappingsPass::createAttributeMappingDriver($mappings)
+            );
+        }
+    }
 }
